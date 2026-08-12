4a448ecf1ac0bdb77518d0aaed87c22856b96d98
Manoj Kumar <manojkumard697@gmail.com>
2026-08-12 09:46:47 +0530

    java Program

---
+++ ManojKumarproject/src/com/basics/TestDemo1.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class TestDemo1 {
+	public void main(String[] args) {
+		System.out.println("Main method started");
+		
+	}
+        static {
+        	System.out.println("static block called");
+        }
+}
